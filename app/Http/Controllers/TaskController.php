fa72a15bed4275cb7376b994485b616a261d91fe
ayaka <ayakaman@example.com>
2023-08-07 01:35:20 +0000

    非ログインTopPageとログイン後TopPageを作成

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Controller;
 class TaskController extends Controller
 {
     /**
-     * タスク一覧ページ を表示する
+     * トップページ を表示する
      *
      * @return \Illuminate\View\View
      */
