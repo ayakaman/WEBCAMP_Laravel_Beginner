66fa4ddee68d42e599905c0352a5a469dc9eb093
ayaka <ayakaman@example.com>
2023-08-18 06:09:07 +0000

    会員登録(簡易)を追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\UserRegisterPost;
+use App\Models\User as UserModel;
 
 
 class UserController extends Controller
@@ -31,6 +32,17 @@ class UserController extends Controller
         // データの取得
         $datum = $request->validated();
         $datum['password'] = Hash::make($datum['password']);
+        //$user = Auth::user();
+        //$id = Auth::id();
+        //var_dump($datum, $user, $id); exit;
+
+        //INSERT
+        try {
+            $r = UserModel::create($datum);
+        } catch(\Throwable $e) {
+            echo $e->getMessage();
+            exit;
+        }
 
         // 認証
         if (Auth::attempt($datum) === false) {
@@ -39,9 +51,9 @@ class UserController extends Controller
                    ;
         }
 
-        //
-        $request->session()->flash('front.user_register_success', true);
-        return redirect()->intended('/');
+         //登録成功
+         $request->session()->flash('front.user_register_success', true);
+         return redirect('/');
     }
 
 }
